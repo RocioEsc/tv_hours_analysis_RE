f4a9656a5b5374ef8168699277c1ac77db597903
EscobarRocio <rocio4gis@gmail.com>
2024-04-17 12:25:51 +0200

    Longer name

--- main.R
+++ main.R
@@ -2,9 +2,9 @@ library(tidyverse)
 library(here)
 
 
-tv_hours_tabulation <- gss_cat %>%
+tv_hours_tabulations <- gss_cat %>%
   filter(age < 30) %>% 
   group_by(marital) %>% 
   summarize (mean_tv_hours = mean(tvhours, na.rm = T ))
   
-write_csv(tv_hours_tabulation, here("TV_hours_by_Marital.csv"))
+write_csv(tv_hours_tabulations, here("TV_hours_by_Marital.csv"))
